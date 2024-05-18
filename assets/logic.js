3cfc5e0be41a7712fb73f9c26d1fce34305c7d90
hannahschwen <hannahschwen@gmail.com>
2024-05-18 16:37:42 -0400

    fetched url, constructed current weather card in js

--- assets/logic.js
+++ assets/logic.js
@@ -25,4 +25,60 @@ searchButton.addEventListener('click', fetchWeather);
 
 function fetchWeather() {
   console.log('clicked');
+
+  let cityInput = document.getElementById('city-input').value;
+  // TODO: convert city to lat and lon - I saw there is an easy way to do this with OpenWeather geo thing
+
+  let key = "2f93013543aedabf89d7193f3daf51f3";
+  let units = "imperial";
+  // I just put in an example lat and lon for now
+  let lat = "51.5073219";
+  let lon = "-0.1276474";
+  let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=${units}`;
+
+  fetch(url)
+    // return results in JSON file
+    .then(response => {
+      return response.json();
+    })
+    // TODO: pass the data to the showWeather function
+    .then ((data) => {
+      showWeather(data);
+    })
+}
+
+// My brain is currently exhausted, but I think something like this should go in a for loop. Maybe I can append it in a separate function so I don't have to separate the current data from the forecast data. I'm sure there's a better way to do this, I'm just tired.
+function showWeather(response) {
+  console.log(response)
+  // construct card for current weather
+  // HTML container for current weather card
+  const currentContainer = document.getElementById('today-container');
+  // card for current weather
+  const currentCard = document.createElement("div");
+  // container for city name, date, icon
+  const currentHeader = document.createElement("div");
+  const cityNameEl = document.createElement("h2");
+  const currentDateEl = document.createElement("h2");
+  const currentIconEl = document.createElement("img");
+  const currentTempEl = document.createElement("p");
+  const currentWindEl = document.createElement("p");
+  const currentHumidityEl = document.createElement("p");
+
+  // add classes
+  currentHeader.classList.add("today-header");
+  currentIconEl.classList.add("icon-today");
+
+  // add content
+  // I put in filler info for now. Icon source isn't working
+  cityNameEl.textContent = "City Name";
+  currentDateEl.textContent = "(9/13/2022)";
+  currentIconEl.src = "./images/filler-icon.png";
+  currentTempEl.textContent = "Temp: 76.62";
+  currentWindEl.textContent = "Wind: 8.43";
+  currentHumidityEl.textContent ="Humidity: 44%";
+
+  currentHeader.append(cityNameEl, currentDateEl, currentIconEl);
+  currentCard.append(currentHeader, currentTempEl, currentWindEl, currentHumidityEl);
+  currentContainer.append(currentCard);
+
 }
